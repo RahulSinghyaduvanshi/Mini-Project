1ee3b40d2b1fada38b821f455e5ddf812e84d481
RahulSinghyaduvanshi <105847387+RahulSinghyaduvanshi@users.noreply.github.com>
2023-11-22 00:14:26 +0530

    Create heart_disease_web_app.py

---
+++ heart_disease_web_app.py
@@ -0,0 +1,7 @@
+echo "# Mini-Project" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/RahulSinghyaduvanshi/Mini-Project.git
+  git push -u origin main
